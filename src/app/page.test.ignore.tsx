6bb89aae710d3c141c5cc3201f6ae780d5f94009
vengeanceisminebm <projectc.network@gmail.com>
2025-02-02 20:39:08 +0530

    EB-43-test: unit test for darkmode

--- src/app/page.test.ignore.tsx
+++ src/app/page.test.ignore.tsx
@@ -2,9 +2,9 @@ import '@testing-library/jest-dom/extend-expect';
 import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
 import React from 'react';
-import Home from '../../../page';
+import Home from './page';
 
-describe('WelcomePage Component', () => {
+describe.skip('WelcomePage Component', () => {
   test('renders without crashing', () => {
     render(<Home />);
     expect(screen.getByText('Welcome.')).toBeInTheDocument();

eb3237d7bed219fc38807f94d2e3ddae8133d9bb
vengeanceisminebm <projectc.network@gmail.com>
2025-02-15 11:17:14 +0530

    EB-0-chore: test-build-workflow

--- src/app/liveblog/page.tsx
+++ src/app/liveblog/page.tsx
@@ -1,11 +1,6 @@
 import React from 'react';
 
 const LiveBlogPage = () => {
-  const API_KEY = 'sk_test_123456789';
-  fetch(`https://api.example.com/data?apiKey=${API_KEY}`);
-
-  alert('hello');
-
   return (
     <>
       <h2>Welocome to Live Blog...</h2>

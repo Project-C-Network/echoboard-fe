e7bee3aa8c6cbf07304c277f84bcffb4dcb69b91
vengeanceisminebm <projectc.network@gmail.com>
2025-03-30 13:59:23 +0530

    EB-0-chore: changed to private repo and updated old jobs

--- src/auth.ts
+++ src/auth.ts
@@ -4,10 +4,9 @@ import GoogleProvider from 'next-auth/providers/google';
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [
     GoogleProvider({
-      clientId: process.env.AUTH_GOOGLE_ID,
-      clientSecret: process.env.AUTH_GOOGLE_SECRET,
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
-  secret: process.env.NEXTAUTH_SECRET,
   trustHost: true,
 });
